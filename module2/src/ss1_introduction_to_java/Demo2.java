65c9fd6f7a61391ad4926673ba98eba08acea9cc
tranhautan2001 <tranhautanvlog.vn@gmail.com>
2022-09-06 22:57:28 +0700

    nopbai

--- module2/src/ss1_introduction_to_java/Demo2.java
+++ module2/src/ss1_introduction_to_java/Demo2.java
@@ -1,8 +1,5 @@
 package ss1_introduction_to_java;
 
-import com.sun.xml.internal.fastinfoset.util.StringArray;
-
-import java.sql.Array;
 import java.util.Scanner;
 
 public class Demo2 {
@@ -10,23 +7,33 @@ public class Demo2 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhập số");
         int number = scanner.nextInt();
-
-        if (number <=10 && number >= 0) {
-            String[] arrayOne ={"zero", "one","two","three","four","five","six","seven","eight","nice","ten"};
+        String[] arrayOne = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nice", "ten"};
+        String[] arrayTwo = {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+        String[] arrayThree = {"", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eightty", "ninety",};
+        String[] arrayFour = {"", "onehundreds", "twohundreds", "threehundreds", "fourhundreds", "fivehundreds", "sixhundreds", "sevenhundreds", "eighthundreds", "ninehundreds"};
+        if (number <= 10 && number >= 0) {
             System.out.println(arrayOne[number]);
         }
-        if (number>10 && number < 20){
-            String[] arrayTwo ={"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-            System.out.println(arrayTwo[number-11]);
+        if (number > 10 && number < 20) {
+            System.out.println(arrayTwo[number % 11]);
         }
-        if (number<100 ){
-            String[] arrayFour ={"","","twenty","thirty","fourty","fifty","sixty","seventy","eightty","ninety",};
-            System.out.println(arrayFour[number/10]);
+        if (number < 100 && number >= 20) {
+            System.out.print(arrayThree[number / 10] + " " + arrayOne[number % 10]);
         }
-        if (number<1000){
-            String[] arrayThree ={"","onehundreds","twohundreds","threehundreds","fourhundreds","fivehundreds","sixhundreds","sevenhundreds","eighthundreds","ninehundreds"};
-            System.out.println(arrayThree[number/100]);
-        }
-        }
+        if (number < 1000 && number >= 100) {
+            if (number % 100 <= 10 && number % 100 >= 0) {
+                System.out.println(arrayFour[number / 100] + " " + arrayOne[number % 100]);
+            }
+            if (number%100 > 10 && number%100 < 20) {
+                System.out.println(arrayFour[number/100]+ " " + arrayTwo[(number%100)%11]);
 
+            }
+            if (number%100 < 100 && number%100 >= 20) {
+                System.out.println(arrayFour[number / 100]+" "+arrayThree[(number%100)/10]+" "+arrayOne[number%10]);
+            }
+
+        }
     }
+
+
+}
--- module2/src/ss1_introduction_to_java/exercis/App_to_read_numbers_into_words/Main.java
+++ module2/src/ss1_introduction_to_java/exercis/App_to_read_numbers_into_words/Main.java
@@ -4,148 +4,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("nhập số cần đọc");
-        Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-        if (number < 20) {
-            switch (number % 10) {
-                case 0:
-                    System.out.println("ten");
-                    break;
-                case 1:
-                    System.out.println("eleven");
-                    break;
-                case 2:
-                    System.out.println("twelve");
-                    break;
-                case 3:
-                    System.out.println("thirteen");
-                    break;
-                case 4:
-                    System.out.println("fourteen");
-                    break;
-                case 5:
-                    System.out.println("fifteen");
-                    break;
-                case 6:
-                    System.out.println("sixteen");
-                    break;
-                case 7:
-                    System.out.println("seventeen");
-                    break;
-                case 8:
-                    System.out.println("eighteen");
-                    break;
-                case 9:
-                    System.out.println("nineteen");
-                    break;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhập số");
+        int number = scanner.nextInt();
+        String[] arrayOne = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nice", "ten"};
+        String[] arrayTwo = {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+        String[] arrayThree = {"", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eightty", "ninety",};
+        String[] arrayFour = {"", "onehundreds", "twohundreds", "threehundreds", "fourhundreds", "fivehundreds", "sixhundreds", "sevenhundreds", "eighthundreds", "ninehundreds"};
+        if (number <= 10 && number >= 0) {
+            System.out.println(arrayOne[number]);
+        }
+        if (number > 10 && number < 20) {
+            System.out.println(arrayTwo[number % 11]);
+        }
+        if (number < 100 && number >= 20) {
+            System.out.print(arrayThree[number / 10] + " " + arrayOne[number % 10]);
+        }
+        if (number < 1000 && number >= 100) {
+            if (number % 100 <= 10 && number % 100 >= 0) {
+                System.out.println(arrayFour[number / 100] + " " + arrayOne[number % 100]);
             }
-        } else if (number < 1000) {
-            String dozen;
-            String units;
-            String hundreds;
-
-            switch (number / 100) {
-                case 1:
-                    hundreds = "onehundreds";
-                    break;
-                case 2:
-                    hundreds = "twohundreds";
-                    break;
-                case 3:
-                    hundreds = "threehundreds";
-                    break;
-                case 4:
-                    hundreds = "fourhundreds";
-                    break;
-                case 5:
-                    hundreds = "fivehundreds";
-                    break;
-                case 6:
-                    hundreds = "sixhundreds";
-                    break;
-                case 7:
-                    hundreds = "sevenhundreds";
-                    break;
-                case 8:
-                    hundreds = "eighthundreds";
-                    break;
-                case 9:
-                    hundreds = "ninehundreds";
-                    break;
-
-                default:
-                    hundreds = "";
+            if (number%100 > 10 && number%100 < 20) {
+                System.out.println(arrayFour[number/100]+ " " + arrayTwo[(number%100)%11]);
 
             }
-            switch ((number % 100)/10) {
-                case 1:
-                    dozen = "onety";
-                    break;
-                case 2:
-                    dozen = "twenty";
-                    break;
-                case 3:
-                    dozen = "thirty";
-                    break;
-                case 4:
-                    dozen = "fourty";
-                    break;
-                case 5:
-                    dozen = "fifty";
-                    break;
-                case 6:
-                    dozen = "sixty";
-                    break;
-                case 7:
-                    dozen = "seventy";
-                    break;
-                case 8:
-                    dozen = "eightty";
-                    break;
-                case 9:
-                    dozen = "ninety";
-                    break;
-                default:
-                    dozen = "";
+            if (number%100 < 100 && number%100 >= 20) {
+                System.out.println(arrayFour[number / 100]+" "+arrayThree[(number%100)/10]+" "+arrayOne[number%10]);
             }
-            switch ((number % 100) % 10) {
-                case 1:
-                    units = "one";
-                    break;
-                case 2:
-                    units = "two";
-                    break;
-                case 3:
-                    units = "three";
-                    break;
-                case 4:
-                    units = "four";
-                    break;
-                case 5:
-                    units = "five";
-                    break;
-                case 6:
-                    units = "six";
-                    break;
-                case 7:
-                    units = "seven";
-                    break;
-                case 8:
-                    units = "eight";
-                    break;
-                case 9:
-                    units = "nine";
-                    break;
 
-                default:
-                    units = "";
-
-            }
-            if (!hundreds.equals("") || !dozen.equals("") || !units.equals(""))
-                System.out.printf("The  '%d' is %s ", number, hundreds + " " + dozen + " " + units);
-            else System.out.print("Invalid input!");
         }
     }
-
 }
+
