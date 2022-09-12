bc0a04c460a397a21a2cc098826a1e0434cc7acf
tranhautan2001 <tranhautanvlog.vn@gmail.com>
2022-09-12 16:02:48 +0700

    nopbai

---
+++ module2/src/ss5__access_modifier_static_property/exercis/access_modifier/Circle.java
@@ -0,0 +1,22 @@
+package ss5__access_modifier_static_property.exercis.access_modifier;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    Circle() {
+
+    }
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.pow(this.radius, 2) * Math.PI;
+    }
+}
---
+++ module2/src/ss5__access_modifier_static_property/exercis/classes_in_java/Test.java
@@ -0,0 +1,9 @@
+package ss5__access_modifier_static_property.exercis.classes_in_java;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setClasses("Hải Trương");
+        student.setName("Quang Nguyễn");
+    }
+}
